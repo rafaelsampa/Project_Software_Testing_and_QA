52a8f051f4fab398700148f468a70a30321339ad
LucasMouratoCH <132353766+LucasMChave@users.noreply.github.com>
2026-05-28 11:24:59 -0300

    garantia

--- tests/test_cardapio.py
+++ tests/test_cardapio.py
@@ -6,7 +6,7 @@ from app import services as s
 # História 1: Cliente visualiza o cardápio
 def test_visualizar_cardapio():
     cardapio = r.listar_cardapio()
-    assert len(cardapio) == 4
+    assert len(cardapio) == 5
     assert cardapio[0].nome == "Hamburguer"
 
 # História 2: Cliente filtra opções vegetarianas
